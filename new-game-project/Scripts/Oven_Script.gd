bb29408a603221ce349ddb5481d92defe071bf86
Valcore737 <v737firas@duck.com>
2025-02-02 13:48:21 -0800

    oven bug fixes and win text and reset

--- new-game-project/Scripts/Oven_Script.gd
+++ new-game-project/Scripts/Oven_Script.gd
@@ -1,11 +1,16 @@
 extends Area2D
  
-@export var oven_door: AnimatedSprite2D
+@onready var oven_door = $AnimatedSprite2D
+@onready var win_text = $Label
+@onready var Timer2 = $Timer2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-
+	if (win_text):
+		win_text.visible = false
+	else:
+		push_error("win text label not assigned!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,5 +19,11 @@ func _process(delta: float) -> void:
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		if (oven_door):
-			print("oven")
+			#print("oven")
 			oven_door.play("Oven_Door")
+		if (win_text):
+			win_text.visible = true
+		Timer2.start()
+			
+func _on_timer_2_timeout() -> void:
+	get_tree().reload_current_scene()

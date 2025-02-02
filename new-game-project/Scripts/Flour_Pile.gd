3bfbe36a54be753939a2cfea79efa9435a65b9c6
Valcore737 <v737firas@duck.com>
2025-02-02 01:09:30 -0800

    Flour Puff sprite

--- new-game-project/Scripts/Flour_Pile.gd
+++ new-game-project/Scripts/Flour_Pile.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-
+@export var smoke: AnimatedSprite2D
 
 var flour_meter_path: NodePath
 var flour_meter: ProgressBar
+#var smoke: AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,6 +13,8 @@ func _ready() -> void:
 	if get_parent().has_meta("Flour_Meter"):
 		flour_meter_path = get_parent().get_meta("Flour_Meter")
 		flour_meter = get_parent().get_node(flour_meter_path)
+		
+	
 	
 
 
@@ -23,4 +26,6 @@ func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		#print("collision")
 		flour_meter.flour = 100.0
+		if (smoke):
+			smoke.play("Smoke")
 		

915086f17065b6849424d7826b4bd9e6d3dc145f
Valcore737 <v737firas@duck.com>
2025-02-02 00:52:31 -0800

    Flour_Pile script
    Flour_Pile script
    & added the flour meter to the game scene

---
+++ new-game-project/Scripts/Flour_Pile.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+
+
+var flour_meter_path: NodePath
+var flour_meter: ProgressBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	
+	if get_parent().has_meta("Flour_Meter"):
+		flour_meter_path = get_parent().get_meta("Flour_Meter")
+		flour_meter = get_parent().get_node(flour_meter_path)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body):
+	if (body.name == "CharacterBody2D"):
+		#print("collision")
+		flour_meter.flour = 100.0
+		

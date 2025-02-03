fc90fc228b6ca0478853134237a2c7f4f88851b4
MichaelDLJones <mjones21575@gmail.com>
2025-02-02 21:05:12 -0800

    MM

---
+++ new-game-project/Scenes/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	# Connect buttons to functions
+	$VBoxContainer/StartButton.connect("pressed", _on_start_button_pressed)
+	$VBoxContainer/QuitButton.connect("pressed", _on_quit_button_pressed)
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")  # Change to your game scene
+
+func _on_quit_button_pressed():
+	get_tree().quit()  # Exits the game

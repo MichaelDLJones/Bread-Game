3604d28a653ed7dbe68acee3deba3bfe555bdf76
Valcore737 <v737firas@duck.com>
2025-02-02 21:27:38 -0800

    boot to main menu

--- new-game-project/Scripts/Oven_Script.gd
+++ new-game-project/Scripts/Oven_Script.gd
@@ -32,4 +32,4 @@ func _on_body_entered(body):
 		Timer2.start()
 			
 func _on_timer_2_timeout() -> void:
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
